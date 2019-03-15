a4d7022e66138ab02cd2ecf7e35db986bfae83fc
Dean4iq <denis.ua22@gmail.com>
2019-03-15 16:18:24 +0200

    New fields added

--- src/main/java/model/entity/Cart.java
+++ src/main/java/model/entity/Cart.java
@@ -1,7 +1,10 @@
 package model.entity;
 
+import java.util.List;
+
 public class Cart {
     private Ticket ticket;
+    private List<Excursion> excursionList;
 
     public Ticket getTicket() {
         return ticket;
@@ -9,5 +12,13 @@ public class Cart {
 
     public void setTicket(Ticket ticket) {
         this.ticket = ticket;
+    }
+
+    public List<Excursion> getExcursionList() {
+        return excursionList;
+    }
+
+    public void setExcursionList(List<Excursion> excursionList) {
+        this.excursionList = excursionList;
     }
 }
